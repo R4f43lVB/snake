af176bb23166983e464419ae5bc98335f39c112c
R4f43lVB <rafaelvb@al.insper.edu.br>
2024-05-08 16:18:28 -0300

    Posição inicial do player, criação da cabeça da cobra, frutas e início do loop do jogo

--- Jogo.py
+++ Jogo.py
@@ -6,7 +6,7 @@ pygame.init()
 
 # Configurações da janela do jogo
 janela_comp=1080
-janela_alt=720
+janela_alt=600
 
 janela=pygame.display.set_mode((janela_comp,janela_alt))
 pygame.display.set_caption('Jogo da cobrinha')
@@ -17,14 +17,65 @@ fps=60
 def baixar_assets():
     assets={}
 
-    assets['corpo']=pygame.image.load('/assets/EL cuerpo.png').convert()
-    assets['cabeca']=pygame.image.load('/assets/cabeça da minha cobra.png')
-    assets['comida']=pygame.image.load('/assets/Fruta.png')
+    assets['corpo']=pygame.image.load('assets/EL cuerpo.png').convert()
+    assets['cabeca']=pygame.image.load('assets/cabeça da minha cobra.png').convert()
+    assets['comida']=pygame.image.load('assets/Fruta.png').convert()
     return assets
 
+# Posição inicial do jogador
+
+p0_cab=[200,300] # POsição da cabeça
+p0_corpo=[[200,300],
+          [190,300],
+          [180,300],
+          [170,300]]
 class snake_head(pygame.sprite.Sprite):
-    def __init__(self,assets):
+    def __init__(self,assets,grupos):
 
         pygame.sprite.Sprite.__init__(self)
         self.sprite=assets['corpo']
-        
+        self.rect=self.sprite.get_rect()
+        self.rect.x=200
+        self.rect.y=300
+        self.speedx=0
+        self.speedy=0
+
+
+class fruta(pygame.sprite.Sprite):
+    def __init__(self,assets):
+        pygame.sprite.Sprite.__init__(self)
+
+        self.sprite=assets['comida']
+        self.pos_x=random.randrange(1,(janela_comp//10)*10)
+        self.pos_y=random.randrange(1,(janela_alt//10)*10)
+
+def jogar(tela):
+    clock=pygame.time.Clock()
+    assets=baixar_assets()
+    
+    cobra=pygame.sprite.Group()
+    tudo=pygame.sprite.Group()
+    
+    grupos={}
+    grupos['cobra']=cobra
+    grupos['tudo']=tudo
+
+    # Criando o player
+    player=snake_head(assets,grupos)
+    tudo.add(player)
+
+    FINAL=0
+    JOGANDO=1
+    MORRENDO=2
+    CRESCENDO=3
+    estado=JOGANDO
+
+    teclas={}
+    pontos=0
+
+    while estado!=FINAL:
+        clock.tick(fps)
+
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                estado=FINAL
