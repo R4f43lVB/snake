511a0d1274062509a617e431ecf79e3f67db59d3
R4f43lVB <rafaelvb@al.insper.edu.br>
2024-05-21 16:32:44 -0300

    Tela de título e seleção de dificuldade

--- Jogo.py
+++ Jogo.py
@@ -102,10 +102,7 @@ def titulo():
     while tela:
         for event in pygame.event.get():
             if event.type==pygame.QUIT:
-                som_colisao.play()
-                wait(2000)
                 pygame.quit()
-                som_colisao.play()
                 quit()
             if event.type==pygame.KEYDOWN:
                 if event.key==pygame.K_RETURN:
@@ -119,17 +116,24 @@ def titulo():
                 elif event.key==pygame.K_9:
                     fps=70
 
-        janela.fill((0,0,0))
+        janela.fill((0,145,145))
         fonte_tit=pygame.font.SysFont('comic sans', 50)
         tex_tit=fonte_tit.render('Jogo da Cobrinha', True, (255,255,255))
-        janela.blit(tex_tit,(200,200))
-        sel_font=pygame.font.SysFont('comic sans', 50)
-        esc_font=pygame.font.SysFont('comic sans', 20)
+        sel_font=pygame.font.SysFont('comic sans', 35)
+        esc_font=pygame.font.SysFont('comic sans', 25)
         sel_texto=sel_font.render('Escolha a dificuldade: ',True,(255,255,255))
-        facil_texto=sel_font.render('1 - Fácil',True,(255,255,255))
-        medio_texto=sel_font.render('2 - Médio',True,(255,255,255))
-        dif_texto=sel_font.render('3 - Difícil',True,(255,255,255))
-        imp_texto=sel_font.render('9 - Impossível (É SÉRIO!)',True,(255,255,255))
+        facil_texto=esc_font.render('1 - Fácil',True,(255,255,255))
+        medio_texto=esc_font.render('2 - Médio',True,(255,255,255))
+        dif_texto=esc_font.render('3 - Difícil',True,(255,255,255))
+        imp_texto=esc_font.render('9 - Impossível (É SÉRIO!)',True,(255,255,255))
+        next_texto=sel_font.render('Agora aperte Enter',True,(255,255,255))
+        janela.blit(tex_tit,(100,50))
+        janela.blit(sel_texto,(70,130))
+        janela.blit(facil_texto,(70,180))
+        janela.blit(medio_texto,(70,210))
+        janela.blit(dif_texto,(70,240))
+        janela.blit(imp_texto,(70,270))
+        janela.blit(next_texto,(70,300))
                 
         pygame.display.flip()
 
